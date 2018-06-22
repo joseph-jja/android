315ca385834b8569e2b816698d9bf3d13040873c
Joe Acosta <josepha48@gmail.com>
2018-06-22 14:24:26 -0700

    rename things for android studio updates

--- SimpleBARTInfo/src/com/ja/sbi/bart/api/BaseDownloader.java
+++
@@ -1,9 +0,0 @@
-package com.ja.sbi.bart.api;
-
-import com.ja.net.ContentRetriever;
-
-public class BaseDownloader {
-
-	public static final ContentRetriever retriever = new ContentRetriever();
-
-}
