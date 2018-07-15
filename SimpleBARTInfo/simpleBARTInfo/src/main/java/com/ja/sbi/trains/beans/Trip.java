fb8141f67826710d94c7c669a315fbad0ae0f725
Joe <josepha48@gmail.com>
2018-07-15 13:22:10 -0700

    initial creation of trip planning bean

---
+++ SimpleBARTInfo/simpleBARTInfo/src/main/java/com/ja/sbi/trains/beans/Trip.java
@@ -0,0 +1,17 @@
+
+import java.util.List;
+
+public class Trip {
+
+    private String originTime;
+    private String originDate;
+    private String destinationTime;
+    private String destinationDate;
+
+    private String origin;
+    private String destination;
+    
+    private List<String> legs;
+    
+    private Fare fareDetails;
+}
