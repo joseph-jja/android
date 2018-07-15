885770afd25a5c088f14fe250b389a83b03c4028
Joe <josepha48@gmail.com>
2018-07-15 16:47:14 -0700

    setters and getters

--- SimpleBARTInfo/simpleBARTInfo/src/main/java/com/ja/sbi/trains/beans/Trip.java
+++ SimpleBARTInfo/simpleBARTInfo/src/main/java/com/ja/sbi/trains/beans/Trip.java
@@ -14,4 +14,36 @@ public class Trip {
     private List<TripLeg> legs;
     
     private Fare fareDetails;
+    
+        public String getDestinationDate() {
+      return this.destinationDate;
+    }
+
+    public void setDestinationDate(String destinationDate) {
+      this.destinationDate = destinationDate;
+    } 
+
+    public String getDestinationTime() {
+      return this.destinationTime;
+    }
+
+    public void setDestinationTime(String destinationTime) {
+      this.destinationTime = destinationTime;
+    } 
+
+    public String getOriginDate() {
+      return this.originDate;
+    }
+
+    public void setOriginDate(String originDate) {
+      this.originDate = originDate;
+    } 
+
+    public String getOriginTime() {
+      return this.originTime;
+    }
+
+    public void setOriginTime(String originTime) {
+      this.originTime = originTime;
+    } 
 }
