3392e64fbf778b6b3e31d31e55ec2d10d33adcd3
Joe <josepha48@gmail.com>
2018-07-15 16:37:28 -0700

    add some getters and setters

--- SimpleBARTInfo/simpleBARTInfo/src/main/java/com/ja/sbi/trains/beans/TripLeg.java
+++ SimpleBARTInfo/simpleBARTInfo/src/main/java/com/ja/sbi/trains/beans/TripLeg.java
@@ -15,4 +15,35 @@ public class TripLeg {
     private String destinationTime;
     private String destinationDate;
 
+    public String getDestination() {
+      return this.destination;
+    }
+
+    public void setDestination(String destination) {
+      this.destination = destination;
+    }
+
+    public String getOrigin() {
+      return this.origin;
+    }
+
+    public void setOrigin(String origin) {
+      this.origin = origin;
+    }
+
+    public String getTransferCode() {
+      return this.transferCode;
+    }
+
+    public void setTransferCode(String transferCode) {
+      this.transferCode = transferCode;
+    }
+
+    public String getOrder() {
+      return this.order;
+    }
+
+    public void setOrder(String order) {
+      this.order = order;
+    }
 }
