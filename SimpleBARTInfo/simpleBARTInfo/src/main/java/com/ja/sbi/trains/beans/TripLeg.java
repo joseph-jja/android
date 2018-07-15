084d3aaa564b7afef52e8a9af057ba3aa56a0898
Joe <josepha48@gmail.com>
2018-07-15 16:21:07 -0700

    create a class to represent a trip leg

---
+++ SimpleBARTInfo/simpleBARTInfo/src/main/java/com/ja/sbi/trains/beans/TripLeg.java
@@ -0,0 +1,18 @@
+
+
+public class TripLeg {
+
+    private String order;
+    private String transferCode;
+    private String origin;
+    private String destination;
+    private String routeLine;
+    private String trainHeadStation;
+
+    // times
+    private String originTime;
+    private String originDate;
+    private String destinationTime;
+    private String destinationDate;
+
+}
