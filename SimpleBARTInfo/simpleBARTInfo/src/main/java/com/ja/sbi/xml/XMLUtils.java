8f735ae9cb79a4acc6ffd868a7aecf305d8c241d
Joe <josepha48@gmail.com>
2018-07-15 17:08:37 -0700

    format and update

--- SimpleBARTInfo/simpleBARTInfo/src/main/java/com/ja/sbi/xml/XMLUtils.java
+++ SimpleBARTInfo/simpleBARTInfo/src/main/java/com/ja/sbi/xml/XMLUtils.java
@@ -1,13 +1,14 @@
-package
+package com.ja.sbi.xml;
 
 public class XMLUtils {
 
-   public String append(String initialString, String xmlData) { 
+	public String append(String initialString, String xmlData) { 
 		
-		if ( initialString != null && initialString.length() > 0 ) {
-			return initialString + xmlData;
-		}
-		return xmlData;		
-	}
-  
+	    if ( initialString != null && initialString.length() > 0 ) {
+		    return initialString + xmlData;
+	    }
+        
+    	return xmlData;		
+    }
+
 }
