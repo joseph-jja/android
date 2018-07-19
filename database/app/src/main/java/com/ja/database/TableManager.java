39f27d9fc1e9e9d6dfeac643c08419d91c5c2036
Joe Acosta <joe.acosta@sephora.com>
2018-07-19 12:04:08 -0700

    update database build for a drop table in TableManager

--- database/app/src/main/java/com/ja/database/TableManager.java
+++ database/app/src/main/java/com/ja/database/TableManager.java
@@ -13,232 +13,241 @@ import android.util.Log;
 
 /**
  * this class is a template for all database table engines
- * 
+ *
  * @author Joseph Acosta
  */
 public class TableManager {
 
-	protected ContentValues ctxValues;
-	protected Table table;
+    protected ContentValues ctxValues;
+    protected Table table;
 
-	// columns key should be name and value is type
-	private Map<String, String> columns = new HashMap<String, String>();;
-	// an entry in requiredFields determines if the field is required or not
-	private Set<String> requiredFields = new HashSet<String>();;
-	// upgradeColumns contains the integer version to alter and the columns to
-	// add
-	private Map<Integer, Map<String, String>> updateColumns;
+    // columns key should be name and value is type
+    private Map<String, String> columns = new HashMap<String, String>();
+    ;
+    // an entry in requiredFields determines if the field is required or not
+    private Set<String> requiredFields = new HashSet<String>();
+    ;
+    // upgradeColumns contains the integer version to alter and the columns to
+    // add
+    private Map<Integer, Map<String, String>> updateColumns;
 
-	private String tableName;
+    private String tableName;
 
-	private final Class<?> _self = getClass();
-	private final String TABLE_TAG = _self.getName();
+    private final Class<?> _self = getClass();
+    private final String TABLE_TAG = _self.getName();
 
-	/**
-	 * constructor so that implementation can do what they need to do takes the
-	 * table name so that it is there for delete
-	 */
-	public TableManager(String tname) {
-		this.ctxValues = new ContentValues();
-		this.tableName = tname;
-	}
+    /**
+     * constructor so that implementation can do what they need to do takes the
+     * table name so that it is there for delete
+     */
+    public TableManager(String tname) {
+        this.ctxValues = new ContentValues();
+        this.tableName = tname;
+    }
 
-	/**
-	 * needed for delete this is the table name
-	 */
-	public String getTableName() {
-		return this.tableName;
-	}
+    /**
+     * needed for delete this is the table name
+     */
+    public String getTableName() {
+        return this.tableName;
+    }
 
-	/**
-	 * this is used by the delete method of crud no need to implement the delete
-	 * as it just returns the id this is then used with the tablename to delete
-	 * by id
-	 */
-	public void setId(int id) {
-		this.table.setId(id);
-	}
+    /**
+     * this is used by the delete method of crud no need to implement the delete
+     * as it just returns the id this is then used with the tablename to delete
+     * by id
+     */
+    public void setId(int id) {
+        this.table.setId(id);
+    }
 
-	public int getId() {
-		return this.table.getId();
-	}
+    public int getId() {
+        return this.table.getId();
+    }
 
-	// method needed to get the actual implementing table
-	public Table getTable() {
-		return this.table;
-	}
+    // method needed to get the actual implementing table
+    public Table getTable() {
+        return this.table;
+    }
 
-	public void dropIfExists() {
-		StringBuilder dropTable = new StringBuilder();
-	     	dropTable.append("DROP TABLE IF EXISTS ");
-	}
-	/* delete method is not needed as there is only delete by id */
-	/* findById method is not needed as it just needs the id */
-	/* find method is not needed as it just needs the column and value */
+    public String dropIfExists() {
+        Log.v(TABLE_TAG, "Entering drop table, for table" + this.tableName);
 
-	/* db creation operations */
-	public String createTable() {
-		Log.v(TABLE_TAG, "Entering create table with column count = " + columns.size());
-		StringBuilder createTable = new StringBuilder();
-		createTable.append("create table " + this.tableName + "(_id integer primary key autoincrement");
-		if (columns.size() > 0) {
-			createTable.append(", ");
-		}
-		Set<Map.Entry<String, String>> fields = columns.entrySet();
-		final int count = fields.size();
-		int index = 0;
-		for (Map.Entry<String, String> column : fields) {
-			createTable.append(" " + column.getKey() + " " + column.getValue().toLowerCase() + " ");
-			if (requiredFields.contains(column.getKey())) {
-				createTable.append(" not null ");
-			}
-			if (index < count - 1) {
-				createTable.append(", ");
-			}
-			index += 1;
-		}
-		createTable.append(");");
-		Log.v(TABLE_TAG, "Exiting create table: " + createTable.toString());
-		return createTable.toString();
-	}
+        StringBuilder dropTable = new StringBuilder();
+        dropTable.append("DROP TABLE IF EXISTS ");
+        dropTable.append(this.tableName);
 
-	public List<String> upgradeTable(int oldVer, int newVer) {
-		Log.v(TABLE_TAG, "Entering update table. Old version = " + oldVer + ". New Version = " + newVer);
-		List<String> updates = new ArrayList<String>();
+        return dropTable.toString();
+    }
+    /* delete method is not needed as there is only delete by id */
+    /* findById method is not needed as it just needs the id */
+    /* find method is not needed as it just needs the column and value */
 
-		Set<Map.Entry<Integer, Map<String, String>>> keys = updateColumns
-				.entrySet();
-		Map<String, String> newColumns = new HashMap<String, String>();
-		for (Map.Entry<Integer, Map<String, String>> version : keys) {
-			if (version.getKey().intValue() > oldVer
-					&& version.getKey().intValue() <= newVer) {
-				newColumns.putAll(version.getValue());
-			}
-		}
-		Set<Map.Entry<String, String>> cols = newColumns.entrySet();
-		for (Map.Entry<String, String> version : cols) {
-			StringBuilder alterTables = new StringBuilder();
-			alterTables.append("ALTER TABLE \"" + this.tableName + "\" ");
-			alterTables.append("ADD COLUMN " + version.getKey() + " ");
-			alterTables.append(version.getValue());
-			if (requiredFields.contains(version.getKey())) {
-				alterTables.append(" not null ");
-			}
-			alterTables.append(" ; ");
-			updates.add(alterTables.toString());
-		}
-		Log.v(TABLE_TAG, "Exiting update table: " + updates.size());
-		return updates;
-	}
+    /* db creation operations */
+    public String createTable() {
+        Log.v(TABLE_TAG, "Entering create table, for table "
+                + this.tableName + " with column count = " + columns.size());
+        StringBuilder createTable = new StringBuilder();
+        createTable.append("CREATE TABLE ");
+        createTable.append(this.tableName);
+        createTable.append("(_id integer primary key autoincrement");
+        if (columns.size() > 0) {
+            createTable.append(", ");
+        }
+        Set<Map.Entry<String, String>> fields = columns.entrySet();
+        final int count = fields.size();
+        int index = 0;
+        for (Map.Entry<String, String> column : fields) {
+            createTable.append(" " + column.getKey() + " " + column.getValue().toLowerCase() + " ");
+            if (requiredFields.contains(column.getKey())) {
+                createTable.append(" not null ");
+            }
+            if (index < count - 1) {
+                createTable.append(", ");
+            }
+            index += 1;
+        }
+        createTable.append(");");
+        Log.v(TABLE_TAG, "Exiting create table: " + createTable.toString());
+        return createTable.toString();
+    }
 
-	private void addToContext(Map.Entry<String, Object> entry, String columnName, String columnType) {
+    public List<String> upgradeTable(int oldVer, int newVer) {
+        Log.v(TABLE_TAG, "Entering update table. Old version = " + oldVer + ". New Version = " + newVer);
+        List<String> updates = new ArrayList<String>();
 
-		Log.v(TABLE_TAG, "Column name is (" + columnName + ") and type is (" + columnType + ")" );
-		final Object value = entry.getValue();
-		if ( value == null ) { return; }
-		if (columnType.equalsIgnoreCase(Table.INTEGER_TYPE)) {
-			Log.v(TABLE_TAG, "Integer type data = " + Integer.valueOf(entry.getValue().toString()));
-			ctxValues.put(columnName, Integer.valueOf(entry.getValue().toString()));
-			Log.v(TABLE_TAG, "Is it in there? " + ctxValues.get(columnName));
-		} else if (columnType.equalsIgnoreCase(Table.STRING_TYPE)) {
-			ctxValues.put(columnName, entry.getValue().toString());
-		} else if (columnType.equalsIgnoreCase(Table.BLOB_TYPE)) {
-			ctxValues.put(columnName, (byte[]) entry.getValue());
-		}
-	}
+        Set<Map.Entry<Integer, Map<String, String>>> keys = updateColumns
+                .entrySet();
+        Map<String, String> newColumns = new HashMap<String, String>();
+        for (Map.Entry<Integer, Map<String, String>> version : keys) {
+            if (version.getKey().intValue() > oldVer
+                    && version.getKey().intValue() <= newVer) {
+                newColumns.putAll(version.getValue());
+            }
+        }
+        Set<Map.Entry<String, String>> cols = newColumns.entrySet();
+        for (Map.Entry<String, String> version : cols) {
+            StringBuilder alterTables = new StringBuilder();
+            alterTables.append("ALTER TABLE \"" + this.tableName + "\" ");
+            alterTables.append("ADD COLUMN " + version.getKey() + " ");
+            alterTables.append(version.getValue());
+            if (requiredFields.contains(version.getKey())) {
+                alterTables.append(" not null ");
+            }
+            alterTables.append(" ; ");
+            updates.add(alterTables.toString());
+        }
+        Log.v(TABLE_TAG, "Exiting update table: " + updates.size());
+        return updates;
+    }
 
-	public ContentValues insert(Map<String, Object> values) {
-		this.ctxValues = new ContentValues();
+    private void addToContext(Map.Entry<String, Object> entry, String columnName, String columnType) {
 
-		// TODO add in required fields check?
-		Set<Map.Entry<String, Object>> entries = values.entrySet();
-		for (Map.Entry<String, Object> entry : entries) {
-			final String columnName = entry.getKey();
-			final String columnType = this.columns.get(columnName);
-			if (!columnName.equalsIgnoreCase(Table.ID_COL)) {
-				this.addToContext(entry, columnName, columnType);
-			}
-		}
-		return this.ctxValues;
-	}
+        Log.v(TABLE_TAG, "Column name is (" + columnName + ") and type is (" + columnType + ")");
+        final Object value = entry.getValue();
+        if (value == null) {
+            return;
+        }
+        if (columnType.equalsIgnoreCase(Table.INTEGER_TYPE)) {
+            Log.v(TABLE_TAG, "Integer type data = " + Integer.valueOf(entry.getValue().toString()));
+            ctxValues.put(columnName, Integer.valueOf(entry.getValue().toString()));
+            Log.v(TABLE_TAG, "Is it in there? " + ctxValues.get(columnName));
+        } else if (columnType.equalsIgnoreCase(Table.STRING_TYPE)) {
+            ctxValues.put(columnName, entry.getValue().toString());
+        } else if (columnType.equalsIgnoreCase(Table.BLOB_TYPE)) {
+            ctxValues.put(columnName, (byte[]) entry.getValue());
+        }
+    }
 
-	public ContentValues update(Map<String, Object> values) {
-		this.ctxValues = new ContentValues();
+    public ContentValues insert(Map<String, Object> values) {
+        this.ctxValues = new ContentValues();
 
-		// TODO add in required fields check?
-		Set<Map.Entry<String, Object>> entries = values.entrySet();
-		for (Map.Entry<String, Object> entry : entries) {
-			final String columnName = entry.getKey();
-			final String columnType = (columnName.equalsIgnoreCase(Table.ID_COL)) ? Table.INTEGER_TYPE : this.columns.get(columnName);
-			this.addToContext(entry, columnName, columnType);
-		}
-		return this.ctxValues;
-	}
+        // TODO add in required fields check?
+        Set<Map.Entry<String, Object>> entries = values.entrySet();
+        for (Map.Entry<String, Object> entry : entries) {
+            final String columnName = entry.getKey();
+            final String columnType = this.columns.get(columnName);
+            if (!columnName.equalsIgnoreCase(Table.ID_COL)) {
+                this.addToContext(entry, columnName, columnType);
+            }
+        }
+        return this.ctxValues;
+    }
 
-	/* used in the find method to map a table row */
-	public Table mapTableRow(Cursor csr) {
+    public ContentValues update(Map<String, Object> values) {
+        this.ctxValues = new ContentValues();
 
-		Table table = new Table();
-		final int cnlen = csr.getColumnCount(); // column count
-		for (int i = 0; i < cnlen; i += 1) {
-			final String colName = csr.getColumnName(i);
-			final String columnType = (colName.equalsIgnoreCase(Table.ID_COL)) ? Table.INTEGER_TYPE : this.columns.get(colName);
-			Log.v(TABLE_TAG, "Column Type is: " + columnType);
-			if (columnType != null) {
-				if (columnType.equalsIgnoreCase(Table.INTEGER_TYPE)) {
-					table.setColumnValue(colName, (Object) Integer.valueOf(csr.getInt(i)));
-				} else if (columnType.equalsIgnoreCase(Table.STRING_TYPE)) {
-					table.setColumnValue(colName, (Object) csr.getString(i));
-				} else if (columnType.equalsIgnoreCase(Table.BLOB_TYPE)) {
-					table.setColumnValue(colName, (Object) csr.getBlob(i));
-				}
-			}
-		}
-		return table;
-	}
+        // TODO add in required fields check?
+        Set<Map.Entry<String, Object>> entries = values.entrySet();
+        for (Map.Entry<String, Object> entry : entries) {
+            final String columnName = entry.getKey();
+            final String columnType = (columnName.equalsIgnoreCase(Table.ID_COL)) ? Table.INTEGER_TYPE : this.columns.get(columnName);
+            this.addToContext(entry, columnName, columnType);
+        }
+        return this.ctxValues;
+    }
 
-	/**
-	 * @param columns
-	 *            the columns to set
-	 */
-	public void setColumns(Map<String, String> columns) {
-		this.columns = columns;
-	}
+    /* used in the find method to map a table row */
+    public Table mapTableRow(Cursor csr) {
 
-	/**
-	 * @return the columns
-	 */
-	public Map<String, String> getColumns() {
-		return columns;
-	}
+        Table table = new Table();
+        final int cnlen = csr.getColumnCount(); // column count
+        for (int i = 0; i < cnlen; i += 1) {
+            final String colName = csr.getColumnName(i);
+            final String columnType = (colName.equalsIgnoreCase(Table.ID_COL)) ? Table.INTEGER_TYPE : this.columns.get(colName);
+            Log.v(TABLE_TAG, "Column Type is: " + columnType);
+            if (columnType != null) {
+                if (columnType.equalsIgnoreCase(Table.INTEGER_TYPE)) {
+                    table.setColumnValue(colName, (Object) Integer.valueOf(csr.getInt(i)));
+                } else if (columnType.equalsIgnoreCase(Table.STRING_TYPE)) {
+                    table.setColumnValue(colName, (Object) csr.getString(i));
+                } else if (columnType.equalsIgnoreCase(Table.BLOB_TYPE)) {
+                    table.setColumnValue(colName, (Object) csr.getBlob(i));
+                }
+            }
+        }
+        return table;
+    }
 
-	/**
-	 * @param requiredFields
-	 *            the requiredFields to set
-	 */
-	public void setRequiredFields(Set<String> requiredFields) {
-		this.requiredFields = requiredFields;
-	}
+    /**
+     * @param columns the columns to set
+     */
+    public void setColumns(Map<String, String> columns) {
+        this.columns = columns;
+    }
 
-	/**
-	 * @return the requiredFields
-	 */
-	public Set<String> getRequiredFields() {
-		return requiredFields;
-	}
+    /**
+     * @return the columns
+     */
+    public Map<String, String> getColumns() {
+        return columns;
+    }
 
-	/**
-	 * @param updateColumns
-	 *            the updateColumns to set
-	 */
-	public void setUpdateColumns(Map<Integer, Map<String, String>> updateColumns) {
-		this.updateColumns = updateColumns;
-	}
+    /**
+     * @param requiredFields the requiredFields to set
+     */
+    public void setRequiredFields(Set<String> requiredFields) {
+        this.requiredFields = requiredFields;
+    }
 
-	/**
-	 * @return the updateColumns
-	 */
-	public Map<Integer, Map<String, String>> getUpdateColumns() {
-		return updateColumns;
-	}
+    /**
+     * @return the requiredFields
+     */
+    public Set<String> getRequiredFields() {
+        return requiredFields;
+    }
+
+    /**
+     * @param updateColumns the updateColumns to set
+     */
+    public void setUpdateColumns(Map<Integer, Map<String, String>> updateColumns) {
+        this.updateColumns = updateColumns;
+    }
+
+    /**
+     * @return the updateColumns
+     */
+    public Map<Integer, Map<String, String>> getUpdateColumns() {
+        return updateColumns;
+    }
 }
