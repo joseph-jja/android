e011e834e3f7efbb8dd4625ab92bd42849b05e64
Joe Acosta <josepha48@gmail.com>
2018-07-27 16:34:34 -0700

    update for export

--- MinnowRSS/minnowRSS/src/main/java/com/ja/minnow/listeners/ImportExportListener.java
+++ MinnowRSS/minnowRSS/src/main/java/com/ja/minnow/listeners/ImportExportListener.java
@@ -13,8 +13,10 @@ import com.ja.minnow.Constants;
 import com.ja.minnow.MinnowRSS;
 import com.ja.dialog.LoadingSpinner;
 import com.ja.minnow.R;
+import com.ja.minnow.tables.FeedsTableData;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ImportExportListener implements Runnable {
@@ -26,29 +28,69 @@ public class ImportExportListener implements Runnable {
     private final Class<?> _self = getClass();
     private final String TAG = _self.getName();
 
-    private static final String backupFilename = "/MinnowRSS/feedlist.txt";
+    private static final String backupFilename = "MinnowRSS.csv";
+
+    // default this to export
+    private static boolean isExport = true;
 
     public void exportFeeds(MinnowRSS activity) {
 
         ImportExportListener.context = activity;
+        ImportExportListener.isExport = true;
 
-        dialog = new LoadingSpinner((Context)activity, "Collecting feeds, please wait.");
+        dialog = new LoadingSpinner((Context) activity, "Collecting feeds, please wait.");
+
+        new Thread(this).start();
+    }
+
+    public void importFeeds(MinnowRSS activity) {
+
+        ImportExportListener.context = activity;
+        ImportExportListener.isExport = false;
+
+        dialog = new LoadingSpinner((Context) activity, "Collecting feeds, please wait.");
 
         new Thread(this).start();
     }
 
     public void run() {
         try {
-            feedsList = Constants.getFeedsservice().listFeeds(ImportExportListener.context);
-            Log.d(TAG, "Got some data " + Integer.valueOf(feedsList.size()).toString());
             File exd = ImportExportListener.context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
             File sd = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            if (sd != null && sd.canWrite()) {
+
+            String state = Environment.getExternalStorageState();
+
+
+            File minnowRSSData = new File(sd, backupFilename);
+            if (minnowRSSData != null && minnowRSSData.canWrite()) {
                 Log.d(TAG, "Can write " + sd);
             }
+            Log.d(TAG, "Or something " + Environment.isExternalStorageEmulated());
             if (exd != null && exd.canWrite()) {
-                Log.d(TAG, "Or write write " + exd);
+                Log.d(TAG, "Or write " + exd);
             }
+
+            if (ImportExportListener.isExport) {
+                feedsList = Constants.getFeedsservice().listFeeds(ImportExportListener.context);
+                Log.d(TAG, "Got some data " + Integer.valueOf(feedsList.size()).toString());
+
+                // need to convert the data into something useful
+                StringBuilder results = new StringBuilder();
+                for ( Table tbl : feedsList) {
+                    final String feedName = "\"" +  tbl.getColumnValue(FeedsTableData.NAME_COL).toString() + "\"";
+                    final String feedUrl = "\"" + tbl.getColumnValue(FeedsTableData.URL_COL).toString() + "\"";
+                    // write data in csv format
+                    final String feedLine = feedName + "," + feedUrl + System.getProperty("line.separator");
+                    results.append(feedLine);
+                }
+                // TODO actually write this out somewhere
+            } else {
+
+                feedsList = new ArrayList<Table>();
+                // TODO read the data file in, if it exists and then add the tables in
+                // should compare data
+            }
+
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
