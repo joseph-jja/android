909dafdd9ceae9354f18a50e4c03d87e306526b1
Joe Acosta <josepha48@gmail.com>
2018-07-27 14:52:10 -0700

    update update.sh

--- update.sh
+++ update.sh
@@ -11,3 +11,8 @@ done
 SOURCE="./ui_libs/app/build/outputs/aar/ui-libs-debug.aar"
 DESTS="./SimpleBARTInfo/ui-libs-debug/ui-libs-debug.aar ./MinnowRSS/ui-libs-debug/ui-libs-debug.aar"
 
+for S in $DESTS; do 
+   cp "$SOURCE" "$S"
+done
+
+
